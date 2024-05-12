917fa673dc081dc3bff1745a5bb8555d7532f1eb
vananh00123 <diizaumziipun0suy@hotmail.com>
2024-05-12 12:26:07 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const factorialRecursive = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
+const mergeSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
   }
-  return n * factorialRecursive(n - 1);
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+};
+const merge = (left, right) => {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 };
