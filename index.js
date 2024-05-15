efe190471f2fe6b79e81aea4bbac35aa7dec999d
vananh00123 <diizaumziipun0suy@hotmail.com>
2024-05-16 05:51:05 +0700

    update commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+function findMaxConsecutiveOnes(nums) {
+  let maxCount = 0;
+  let count = 0;
+  for (const num of nums) {
+    if (num === 1) {
+      count++;
+      maxCount = Math.max(maxCount, count);
+    } else count = 0;
+  }
+  return maxCount;
+}
